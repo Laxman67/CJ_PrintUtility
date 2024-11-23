225f7cd3ee25099ad9cf4f18b1543e08fd47d210
Laxman <88457639+Laxman67@users.noreply.github.com>
2024-11-23 13:50:42 +0000

    shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { useContext } from 'react';
-import Heading from './components/Heading';
-import Content from './components/Content';
-import SelectOption from './components/SelectOption';
+import { useContext } from "react";
+import Heading from "./components/Heading";
+import Content from "./components/Content";
+import SelectOption from "./components/SelectOption";
 
-import { rowContext } from './context/rowContext';
+import { rowContext } from "./context/rowContext";
+import { Button } from "./components/ui/button";
 
 const App = () => {
-  const { handleInputChange, rows, rate, addRow ,delRow} = useContext(rowContext);
+  const { handleInputChange, rows, rate, addRow, delRow } =
+    useContext(rowContext);
   console.log(rows);
-  
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -21,25 +22,23 @@ const App = () => {
   };
 
   return (
-    <div className='w-[95%] text-center'>
+    <div className="w-[95%] text-center">
       {/* Heading */}
       <Heading />
-
-
 
       {/* Content */}
       <Content />
 
       <form onSubmit={handleSubmit}>
-        <table className='w-[80%] mx-auto mt-2 text-justify border border-gray-500'>
+        <table className="w-[80%] mx-auto mt-2 text-justify border border-gray-500">
           {/* Table Heading Part */}
-          <thead className='uppercase bg-gray-400 border border-gray-500 dark:text-white p-2'>
+          <thead className="uppercase bg-gray-400 border border-gray-500 dark:text-white p-2">
             <tr>
-              <th className='border border-gray-600 text-center'>S.No</th>
-              <th className='border border-gray-600 text-center'>Item Name</th>
-              <th className='border border-gray-600 text-center'>Rate</th>
-              <th className='border border-gray-600 text-center'>Quantity</th>
-              <th className='border border-gray-600 text-center'>Total</th>
+              <th className="border border-gray-600 text-center">S.No</th>
+              <th className="border border-gray-600 text-center">Item Name</th>
+              <th className="border border-gray-600 text-center">Rate</th>
+              <th className="border border-gray-600 text-center">Quantity</th>
+              <th className="border border-gray-600 text-center">Total</th>
             </tr>
           </thead>
 
@@ -48,52 +47,52 @@ const App = () => {
             {rows.map((row, index) => {
               return (
                 <tr key={row.id}>
-                  <td className='border border-gray-600 font-semibold text-center   '>
+                  <td className="border border-gray-600 font-semibold text-center   ">
                     {index + 1}
                   </td>
-                  <td className='border border-gray-600  '>
+                  <td className="border border-gray-600  ">
                     <select
-                      name='itemName'
-                      className='outline-none p-1 hover:bg-gray-100'
+                      name="itemName"
+                      className="outline-none p-1 hover:bg-gray-100"
                       value={row.itemName}
-                      onChange={(e) => handleInputChange(e, index, 'itemName')}
+                      onChange={(e) => handleInputChange(e, index, "itemName")}
                     >
                       <SelectOption />
                     </select>
                   </td>
 
-                  <td className='border border-gray-600  '>
+                  <td className="border border-gray-600  ">
                     <input
-                      type='number'
+                      type="number"
                       value={rate}
                       min={0}
                       max={rate}
-                      onChange={(e) => handleInputChange(e, index, 'rate')}
+                      onChange={(e) => handleInputChange(e, index, "rate")}
                       readOnly
-                      className='outline-none p-1 hover:bg-gray-100'
+                      className="outline-none p-1 hover:bg-gray-100"
                     />
                   </td>
 
-                  <td className='border border-gray-600  '>
+                  <td className="border border-gray-600  ">
                     <input
-                      type='number'
-                      step='0.001' // Allow three decimal places
+                      type="number"
+                      step="0.001" // Allow three decimal places
                       max={9000}
                       min={0}
                       value={row.quantity}
-                      onChange={(e) => handleInputChange(e, index, 'quantity')}
-                      className='outline-none p-1 hover:bg-gray-100'
+                      onChange={(e) => handleInputChange(e, index, "quantity")}
+                      className="outline-none p-1 hover:bg-gray-100"
                     />
                   </td>
 
-                  <td className='border border-gray-600  '>
+                  <td className="border border-gray-600  ">
                     <input
-                      type='number'
+                      type="number"
                       max={9000}
                       min={0}
                       value={row.total}
-                      onChange={(e) => handleInputChange(e, index, 'total')}
-                      className='outline-none p-1 hover:bg-gray-100'
+                      onChange={(e) => handleInputChange(e, index, "total")}
+                      className="outline-none p-1 hover:bg-gray-100"
                       readOnly
                     />
                   </td>
@@ -102,52 +101,49 @@ const App = () => {
             })}
           </tbody>
           <tfoot>
-            <tr className='uppercase bg-gray-400 border font-bold border-gray-500 dark:text-white p-2'>
-              <td className='border border-gray-600   '></td>
-              <td className='border border-gray-600   '></td>
-              <td className='border border-gray-600   '>
-               {rate}
-               
+            <tr className="uppercase bg-gray-400 border font-bold border-gray-500 dark:text-white p-2">
+              <td className="border border-gray-600   "></td>
+              <td className="border border-gray-600   "></td>
+              <td className="border border-gray-600   ">{rate}</td>
+              <td className="border border-gray-600   ">
+                {rows.reduce((acc, curr) => acc + Number(curr.quantity), 0)}
               </td>
-              <td className='border border-gray-600   '>
-              {rows.reduce((acc,curr)=>acc+Number(curr.quantity),0)}
-               
-              </td>
-              <td className='border border-gray-600   '>
+              <td className="border border-gray-600   ">
                 {/* {rows[0].total + rows[1].total} */}
-                {rows.reduce((acc,curr)=>acc+curr.total,0)}
-               
+                {rows.reduce((acc, curr) => acc + curr.total, 0)}
               </td>
             </tr>
           </tfoot>
         </table>
 
-        <button className='mt-4 ml-2 p-2 bg-gray-500  hover:bg-gray-700 text-white rounded print:hidden'>
+        <Button className="mt-4 ml-2 p-2 bg-gray-500  hover:bg-gray-700 text-white rounded print:hidden">
           Submit Certifcate
-        </button>
-        <button
-        className='mt-4 ml-2 p-2 bg-gray-500  hover:bg-gray-700 text-white rounded print:hidden'
-        onClick={() => printCertificate()}
-      >
-        Print Certifcate
-      </button>
+        </Button>
+        <Button
+          className="mt-4 ml-2 p-2 bg-gray-500  hover:bg-gray-700 text-white rounded print:hidden"
+          onClick={() => printCertificate()}
+        >
+          Print Certifcate
+        </Button>
       </form>
 
-      <button
-        className='mt-4 p-2 bg-green-600 hover:bg-green-700 text-white rounded print:hidden'
+      <Button
+        className="mt-4 p-2 bg-green-600 hover:bg-green-700 text-white rounded print:hidden"
         onClick={addRow}
       >
-        Add Row
-      </button>
-      <button
-className={`mt-4 p-2 bg-red-600 hover:bg-red-900 ml-2 text-white rounded print:hidden ${rows && rows.length === 1 ? " hidden" : ""}`}
+        {" "}
+        Add Row{" "}
+      </Button>
 
+      <Button
+        className={`mt-4 p-2 bg-red-600 hover:bg-red-900 ml-2 text-white rounded print:hidden ${
+          rows && rows.length === 1 ? " hidden" : ""
+        }`}
         onClick={delRow}
-        disabled={rows.length===1?true:false}
+        disabled={rows.length === 1 ? true : false}
       >
         Delete Row
-      </button>
-     
+      </Button>
     </div>
   );
 };
